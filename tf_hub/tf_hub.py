2bf408913c4111932bdb08616b81e3daccde56f8
ritsukishuto <rtk410s@gmail.com>
2020-06-20 16:37:23 +0900

    バージョン情報表示コードをコメントアウト

--- tf_hub/tf_hub.py
+++ tf_hub/tf_hub.py
@@ -11,12 +11,12 @@ import tensorflow_hub as hub
 import tensorflow_datasets as tfds
 
 # バージョン情報を表示
-print("---DBG INFO---")
-print("Ver. :", tf.__version__)
-print("Eager mode: ", tf.executing_eagerly())
-print("Hub ver. :", hub.__version__)
-print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
-print("---END---")
+#print("---DBG INFO---")
+#print("Ver. :", tf.__version__)
+#print("Eager mode: ", tf.executing_eagerly())
+#print("Hub ver. :", hub.__version__)
+#print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
+#print("---END---")
 
 # データセットを分割
 # 訓練データを60%と40%に分割, 15,000件
