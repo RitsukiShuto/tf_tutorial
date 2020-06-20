178182e86ca1391f975f44f3f0c4c63b1d80d181
ritsukishuto <rtk410s@gmail.com>
2020-06-20 16:35:54 +0900

    データセット分割用コードを追加

--- tf_hub/tf_hub.py
+++ tf_hub/tf_hub.py
@@ -17,3 +17,12 @@ print("Eager mode: ", tf.executing_eagerly())
 print("Hub ver. :", hub.__version__)
 print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
 print("---END---")
+
+# データセットを分割
+# 訓練データを60%と40%に分割, 15,000件
+# 訓練 => 10,000 テスト => 25,000
+train_data, validation_data, test_data = tfds.load(
+    name = "imdb_reviews",
+    split = ('train[:60%]', 'train[60%:]', 'test'),
+    as_supervised = True)
+
